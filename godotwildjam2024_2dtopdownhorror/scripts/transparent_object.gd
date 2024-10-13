b5ea61471c32a3d53b07bc9c8e827dfddbf8b7e3
kalasherr <kalashandreev@gmail.com>
2024-10-13 16:44:02 +0300

    red square sprite transfer

--- godotwildjam2024_2dtopdownhorror/scripts/transparent_object.gd
+++ godotwildjam2024_2dtopdownhorror/scripts/transparent_object.gd
@@ -1,16 +1,17 @@
 extends Area2D
 
-var fading = ""
+var fading = "unfading"
+
+#ive tried to do something, that is not bound to process(), but all ways exccept this was buggy
+
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func _process(delta):
 	if fading == "fading" and $Sprite.modulate[3] > 0:
 		$Sprite.modulate[3] -= 0.05
 	if fading == "unfading" and $Sprite.modulate[3] < 1:
 		$Sprite.modulate[3] += 0.05
-		
-
 
 func _on_area_entered(area):
 	if area.name == "PlayerTransparencyController":
