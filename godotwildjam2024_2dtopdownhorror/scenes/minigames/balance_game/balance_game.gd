3c990b8378538ce354dee670e189c493aeccf761
kalasherr <kalashandreev@gmail.com>
2024-10-20 16:56:28 +0300

    battery game done

--- godotwildjam2024_2dtopdownhorror/scenes/minigames/balance_game/balance_game.gd
+++ godotwildjam2024_2dtopdownhorror/scenes/minigames/balance_game/balance_game.gd
@@ -4,6 +4,7 @@ var current_mode = "a"
 var lost = false
 var balance = 0
 var fading
+var rnd = RandomNumberGenerator.new()
 func _ready():
 	self.modulate[3] = 0
 	$ValveSprite.frame = 4
@@ -17,25 +18,11 @@ func _process(delta):
 			current_mode = "a"
 		if Input.is_action_just_pressed("move_right"):
 			current_mode = "d"
-		if current_mode == "a":
-			speed -= 0.07
-		if current_mode == "d":
-			speed += 0.07
-		if abs(balance) < 45:
-			balance += speed
-		else:
-			balance = sign(balance) * (abs(balance)  - 1)
-			speed = 0
-			$Timer.paused = true
-		$ValveSprite.frame = round((balance+46))/10 - 1
-		$ProgressBar.frame = 7 - round($Timer.time_left)
 		#fade()
 		#get_parent().get_parent().get_node("Player").controllable = true
 		#queue_free()
 
-func _on_timer_timeout():
-	Global.balance_completed = true
-	fade()
+
 
 func fade():
 	if modulate[3] < 1.0:
